9acb70b5b0240b81c45e1fa9ba7780b3560120ce
ifrof <226154071+ifrof@users.noreply.github.com>
2026-01-29 01:30:14 +0800

    Delete drizzle directory

--- drizzle/migrations/add_two_factor_auth.sql
+++
@@ -1,4 +0,0 @@
--- Add two-factor authentication fields to users table
-ALTER TABLE `users` ADD COLUMN `twoFactorEnabled` INT DEFAULT 0 NOT NULL AFTER `emailVerified`;
-ALTER TABLE `users` ADD COLUMN `twoFactorSecret` VARCHAR(255) AFTER `twoFactorEnabled`;
-ALTER TABLE `users` ADD COLUMN `twoFactorBackupCodes` TEXT AFTER `twoFactorSecret`;
